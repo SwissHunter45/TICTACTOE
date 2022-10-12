36e36f5198afbce655832693a3d5d875f7a7600b
Alen Omicevic <alen.omicevic2@gmail.com>
2022-10-12 15:25:30 -0400

    Recovered Project

--- TIC.py
+++ TIC.py
@@ -1,2 +1,26 @@
-print("New Feature")
-print("Jizz")
+import random
+
+mylist = []
+
+v = True
+j = True
+game_count = 0
+
+while j == True:
+    user_input = int(input(""))
+    mylist.append(user_input)
+    game_count += 1
+    print(mylist)
+
+    while v == True:
+        rng = random.randint(1,9)
+        if rng not in mylist:
+            mylist.append(rng)
+            print(mylist)
+            break
+        else:
+            v == False
+
+        if game_count == 9:
+            print("All nine indexes are filled out")
+            j = False
